b909aab690871bda9519133cf4fec6f7aced3024
Adam Kasztenny <adamkasztenny@gmail.com>
2020-03-05 18:43:19 -0500

    Add Title

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title', () => {
+  const { queryByText } = render(<App />);
+  expect(queryByText('Employee Title Generator')).not.toBeNull();
 });
